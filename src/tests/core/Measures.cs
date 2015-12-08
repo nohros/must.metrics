4ace795aaabe51be676014bbfc43990953621a88
nohros <nohros.ceo@gmail.com>
2015-12-08 19:09:12 -0200

    Added unit tests for Timer metric.

---
+++ src/tests/core/Measures.cs
@@ -0,0 +1,13 @@
+﻿namespace Nohros.Metrics.Tests
+{
+  public static class Measures
+  {
+    public static Measure Get(IMetric metric, MetricContext context) {
+      var step = metric as IStepMetric;
+      if (step == null) {
+        return Testing.Sync<Measure>(metric, metric.GetMeasure, context);
+      }
+      return Testing.Sync<Measure>(step, step.GetMeasure, context, true);
+    }
+  }
+}
