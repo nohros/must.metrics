49be5aa7fc5b0efb330b22f4f586eedbd9b690dd
nohros <nohros.ceo@gmail.com>
2016-06-15 18:27:29 -0300

    Stop the development of the API module

--- src/api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nohros.Metrics.Api.Global" Language="C#" %>
--- src/aspnet/configuration/ISettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Nohros.Metrics
-{
-  public interface ISettings
-  {
-    int ReportingInterval { get; }
-  }
-}
